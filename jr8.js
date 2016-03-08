5b86ab3f8d3abcb9381102e282beb8ac2472c8d2
jiajianrong <jiajianrong@58.com>
2016-03-08 18:47:50 +0800

    plugin updated/added

--- jr8.js
+++ jr8.js
@@ -4,3 +4,19 @@ fis.require.prefixes.unshift('jello');
 
 fis.cli.name = 'jr8';
 fis.cli.info = fis.util.readJSON(__dirname + '/package.json');
+
+
+
+
+var ppArr = fis.config.get('modules.postpackager') || [];
+ppArr.push('vmparse');
+ppArr.push('require-framework');
+fis.config.set('modules.postpackager', ppArr);
+
+/*
+fis.config.merge({
+    modules: {
+        postpackager: ['vmparse','require-framework']
+    }
+});
+*/
