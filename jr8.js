a1cce761d9f1d89161617c9f0c3cb54a852e9565
jiajianrong <jiajianrong@58.com>
2016-03-09 15:46:59 +0800

    performance added

--- jr8.js
+++ jr8.js
@@ -8,15 +8,24 @@ fis.cli.info = fis.util.readJSON(__dirname + '/package.json');
 
 
 
+
 var ppArr = fis.config.get('modules.postpackager') || [];
-ppArr.push('vmparse');
+
+
+/\bpublish\b/i.test(process.title) && ppArr.push('vmparse');
+/\bperformance\b/i.test(process.title) && ppArr.push('performance-framework');
+
+// performance-framework 一定要在 require-framework 之前，因为二者都用到了vm钩子
 ppArr.push('require-framework');
+
+
+
 fis.config.set('modules.postpackager', ppArr);
 
-/*
+/**
 fis.config.merge({
     modules: {
-        postpackager: ['vmparse','require-framework']
+        postpackager: ppArr
     }
 });
-*/
+**/
